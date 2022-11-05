3cdc3a10ecccb7943771784aac0fa6efc8a9741b
MaritaIfemeni <marita.ifemeni@student.hamk.fi>
2022-11-05 22:16:12 +0200

    project

---
+++ BUILD/NUCLEO_L432KC/ARMC6/mbed-os/connectivity/cellular/source/framework/common/CellularLog.d
@@ -0,0 +1,5 @@
+BUILD/NUCLEO_L432KC/ARMC6/mbed-os/connectivity/cellular/source/framework/common/CellularLog.o: \
+  mbed-os/connectivity/cellular/source/framework/common/CellularLog.cpp \
+  BUILD/NUCLEO_L432KC/ARMC6/mbed_config.h \
+  mbed-os/connectivity/cellular/include/cellular/framework/common/CellularLog.h \
+  mbed-os/features/frameworks/mbed-trace/mbed-trace/mbed_trace.h
---
+++ BUILD/NUCLEO_L432KC/ARMC6/mbed-os/drivers/source/usb/OperationListBase.d
@@ -0,0 +1,32 @@
+BUILD/NUCLEO_L432KC/ARMC6/mbed-os/drivers/source/usb/OperationListBase.o: \
+  mbed-os/drivers/source/usb/OperationListBase.cpp \
+  BUILD/NUCLEO_L432KC/ARMC6/mbed_config.h \
+  mbed-os/drivers/include/drivers/internal/OperationListBase.h \
+  mbed-os/drivers/include/drivers/internal/LinkedListBase.h \
+  mbed-os/drivers/include/drivers/internal/LinkEntry.h \
+  mbed-os/drivers/include/drivers/internal/AsyncOp.h \
+  mbed-os/rtos/include/rtos/Mutex.h \
+  mbed-os/rtos/include/rtos/mbed_rtos_types.h \
+  mbed-os/rtos/source/TARGET_CORTEX/rtx5/Include/cmsis_os2.h \
+  mbed-os/rtos/include/rtos/internal/mbed_rtos1_types.h \
+  mbed-os/rtos/source/TARGET_CORTEX/rtx4/cmsis_os.h \
+  mbed-os/rtos/source/TARGET_CORTEX/rtx5/RTX/Include/rtx_os.h \
+  mbed-os/rtos/include/rtos/internal/mbed_rtos_storage.h \
+  mbed-os/rtos/source/TARGET_CORTEX/mbed_rtx_storage.h \
+  mbed-os/rtos/source/TARGET_CORTEX/mbed_rtx_conf.h \
+  mbed-os/targets/TARGET_STM/mbed_rtx.h \
+  mbed-os/rtos/include/rtos/Kernel.h \
+  mbed-os/platform/include/platform/mbed_toolchain.h \
+  mbed-os/platform/include/platform/mbed_preprocessor.h \
+  mbed-os/platform/include/platform/NonCopyable.h \
+  mbed-os/platform/include/platform/ScopedLock.h \
+  mbed-os/rtos/include/rtos/Semaphore.h \
+  mbed-os/platform/include/platform/Callback.h \
+  mbed-os/platform/cxxsupport/mstd_cstddef \
+  mbed-os/platform/cxxsupport/mstd_new \
+  mbed-os/platform/include/platform/mbed_assert.h \
+  mbed-os/platform/cxxsupport/mstd_type_traits \
+  mbed-os/platform/cxxsupport/mstd_functional \
+  mbed-os/platform/cxxsupport/mstd_memory \
+  mbed-os/platform/cxxsupport/mstd_utility \
+  mbed-os/platform/cxxsupport/mstd_iterator
